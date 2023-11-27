046d6ddb20818e0758f6f312f6d29949dd9b7cd6
c23lu <c23lu@uwaterloo.ca>
2023-11-27 18:17:20 -0500

    link impl

--- link.cc
+++ link.cc
@@ -1,1 +1,15 @@
-//this is the .cc for link
+#include "link.h"
+
+Link::Link(): name{' '}, type{' '}, strength{0}, revealed{false}, moveLength{1} {}
+
+char Link::getName() const { return name;}//Returns the name of a Link (i.e. a-h, A-H)
+char Link::getType() const { return type;} //Returns the type of a Link
+int Link::getStrength() const {return strength;} //Returns the strength of a Link
+void Link::giveBoost() {moveLength++;} //increases moveLength by 1 
+void Link::toggleType(){ //changes a virus to a data and vice versa
+    if (type == 'V'){
+        type = 'D';
+    }else if (type == 'D'){
+        type = 'V';
+    }
+} 
--- link.h
+++ link.h
@@ -1,1 +1,18 @@
-//this is the link.h 
+#ifndef __CELL_H__
+#define __CELL_H__
+class Link{
+    char name;
+    char type; 
+    int strength;
+    bool revealed; 
+    int moveLength; 
+    public: 
+    Link(); //default ctor
+    char getName() const; //Returns the name of a Link (i.e. a-h, A-H)
+    char getType() const; //Returns the type of a Link
+    int getStrength() const; //Returns the strength of a Link
+    void giveBoost(); //increases moveLength by 1 
+    void toggleType(); //changes a virus to a data and vice versa
+};
+
+#endif
