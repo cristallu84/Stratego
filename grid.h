408ffd49d36fcea98407c83a18ee128978fce573
c23lu <c23lu@uwaterloo.ca>
2023-11-27 19:56:32 -0500

    .h implementation

---
+++ grid.h
@@ -0,0 +1,31 @@
+#ifndef __GRID_H__
+#define __GRID_H__
+#include "cell.h"
+#include "player.h"
+#include "textdisplay.h"
+
+class Grid{
+std::vector<std::vector<Cell>> theGrid; 
+int gridSize;
+TextDisplay *textDisplay; 
+//GraphicsDisplay *GraphicsDisplay;
+Player player1;
+Player player2; 
+int whoseTurn; 
+public: 
+void init(int n);
+Cell& findCell(char l);
+int getTurn(); 
+void nextTurn(); 
+void move(char l, char dir); 
+void download(Cell& c); 
+void battle(Cell& initi, Cell& fighter);
+void playAbility(int ID, Cell& c);
+void printAbilities(); 
+Player& getPlayer(int n); 
+}
+
+
+
+
+#endif
---
+++ player.h
@@ -0,0 +1,29 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+#include <vector>
+#include <string>
+#include "observer.h"
+
+
+class Player: public Observer{
+    std::vector<char> Abilities; 
+    std::vector<std::string> MyLinks;
+    std::vector<std::string> OppLinks;
+    int MyD;
+    int MyV;
+    int OppD;
+    int OppV;
+    int OppAbil;
+    public: 
+    void notify(Cell &c);
+    bool isWon();
+    int getMyD(); 
+    int getMyV();
+    int getOppD();
+    int getOppV();
+    int getOppAbil();
+    void setAbility(std::string s);
+    void addLink(std::string s);
+};
+
+#endif
