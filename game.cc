e13487ecb907b68b057c386bac2a5f36300cd75c
c23lu <c23lu@uwaterloo.ca>
2023-11-30 22:19:19 -0500

    fix exception errors

--- game.cc
+++ game.cc
@@ -92,54 +92,6 @@ void Grid::init(int n, vector<string> p1_links, vector<string> p2_links) {
 }
 
 
-// Checks if the coordinates given are within the board
-bool Grid::outBound(int r, int c) {
-    if (r >= gridSize || c >= gridSize) {
-        return false;
-    }
-    return true;
-}
-
-
-// Checks if the link exists
-bool Grid::isLink(char c) {
-    if (c >= 41 && c <= 48) {
-        return true;
-    } else if (c >= 61 && c <= 68) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-// Checks if the link is currently on the board
-bool Grid::onBoard(char l) {
-    try {
-        Cell & c = this->findCell(l);
-    } catch (const not_on_board& e) {
-        return false;
-    }
-    return true;
-}
-
-bool Grid::link_in_player(char l, int p) {
-    if (p == 1) {
-        for (int i = 41; i <= 48; ++i) {
-            if (l == i) {
-                return true;
-            }
-        }
-        return false; 
-    } else {
-        for (int i = 61; i <= 68; ++i) {
-            if (l == i) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
 // Returns the cell with link l
 Cell& Grid::findCell(char l) {
     for (int r = 0; r < gridSize; ++r) {
--- game.h
+++ game.h
@@ -15,10 +15,6 @@ class Grid{
     Player player1;
     Player player2; 
     int whoseTurn; 
-    bool outBound(int r, int c); // Checks if out of bounds
-    bool isLink(char l); // Check if link exists
-    bool onBoard(char l); // Check if cell is on the board
-    bool link_in_player(char l, int p); // Check if link is player's link
 
     public:
         Grid();
@@ -35,12 +31,6 @@ class Grid{
         void battle(Cell& init, Cell& fighter); // Handles the battle feature
         std::vector<std::string>& printAbilities(); 
         Player& getPlayer(int n);
-
-        void linkBoost(char c);
-        void firewall(int r, int c);
-        void downloadAbility(char c);
-        void polarize(char c);
-        void scan(char c);
 
         friend std::ostream &operator<<(std::ostream &out, const Grid &g);
 };
